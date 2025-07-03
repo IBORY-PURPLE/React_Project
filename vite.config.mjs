ae5dcb17fc4a66c51facac01d22ba3457a5105bd
채우 <lala1522@gachon.ac.kr>
2025-07-04 01:29:25 +0900

    setting server

---
+++ vite.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default {
+  plugins: [react()],
+
+  server: {
+    host: true,
+    port: 5173
+  }
+}
+
