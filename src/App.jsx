f268d0ab4e1e1cf7b2d072df0a031d25a03e6a6b
채우 <lala1522@gachon.ac.kr>
2025-07-05 15:42:10 +0900

    succesful components lecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
+
 function App() {
+  // useState기본값을 '': 문자열 데이터 0은 숫자 데이터
+  const [selectedTopic, setSelectedTopic] = useState("");
+
+  function onSelect(selectedData) {
+    // 이 코드에서 App컴포넌트 리렌더링 예약을 걸어논 것.
+    // onSelect함수 실행 끝나고 리렌더링 해서 이 함수 내의 console은 이전값을 출력.
+    setSelectedTopic(selectedData);
+    console.log(selectedTopic);
+  }
   return (
     <div>
       <Header />
@@ -10,21 +23,47 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Component</TabButton>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => onSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => onSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => onSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => onSelect("state")}
+            >
+              State
+            </TabButton>
           </menu>
+          {!selectedTopic && <p>please click button.</p>}
+          {selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>{EXAMPLES[selectedTopic].code}</pre>
+            </div>
+          )}
         </section>
         <h2>Time to get started!</h2>
       </main>
